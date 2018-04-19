6b9504dd3ef5195ac07c6403016784047e4d13a3
ssheaks <sarah.sheaks@gmail.com>
2018-04-19 11:10:23 -0700

    add inactivity.js file

---
+++ src/components/inacvitity.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { clearAuth } from '../actions/auth';
+import store from '../store';
+
+export default class Inactivity extends React.Component {
+  //need to receive props, need to render(), need to run logOut() function, need to add litener for activity, need to return empty html element
+}
